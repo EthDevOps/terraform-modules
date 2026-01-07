58b70846aa650f00a851138eceb917621d65bc76
Markus Keil <markus@keil-connect.com>
2026-01-07 14:58:27 +0100

    fix dependson

--- modules/proxmox-vm/netbox.tf
+++ modules/proxmox-vm/netbox.tf
@@ -137,7 +137,7 @@ resource "netbox_mac_address" "vm_eth1" {
 }
 
 resource "netbox_primary_ip" "vm_primary_ip" {
-  depends_on = [netbox_available_ip_address.vm_ip.id,netbox_virtual_machine.vm.id,netbox_interface.vm_eth0]
+  depends_on = [netbox_available_ip_address.vm_ip,netbox_virtual_machine.vm,netbox_interface.vm_eth0]
   ip_address_id      = netbox_available_ip_address.vm_ip.id
   virtual_machine_id = netbox_virtual_machine.vm.id
 }
