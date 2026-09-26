9b83d46d9b605dd1a8f6ca7c0ba0df8549d62545
Markus Keil <markus.keil@ethereum.org>
2026-09-26 10:44:57 +0200

    f

--- modules/netbox-vm/variables.tf
+++ modules/netbox-vm/variables.tf
@@ -65,7 +65,7 @@ variable "services" {
     name  = string
     proto = string
     port = number
-    expose_domain = default(string, "")
+    expose_domain = optional(string, "")
   }))
   default = []
 }
